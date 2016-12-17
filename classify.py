7bf843c138d27fb93bfb26b2138b1c21cb779b9d
Rob <robertipk@gmail.com>
2016-12-17 17:59:04 -0500

    progress

--- classify.py
+++ classify.py
@@ -14,48 +14,86 @@ b = chessdata['label']
 # print(a)
 
 data = csv.reader(open('chessboard.csv','rb'), delimiter=",", quotechar='|')
-A1, B1, A0, B0, kcc, label1, label0 = [], [], [], [], [], [], [], []
+# for scatter plot
+A1, B1, A0, B0, all_samples, label1, label0 = [], [], [], [], [], [], []
+# for SVM
+all_labels = []
 
 # print(data)
 for row in data:
-    kcc.append([row[0],row[1]])
+
+    all_samples.append([row[0],row[1]])
+    all_labels.append(row[2])
     if row[2]=='0':
       A0.append(row[0])
-      B0.append(row[0])
+      B0.append(row[1])
       label0.append(row[2])
     else:
       A1.append(row[0])
       B1.append(row[1])
       label1.append(row[2])
+all_samples.pop(0)
+all_labels.pop(0)
+
 A1.pop(0)
 B1.pop(0)
-kcc1.pop(0)
 label1.pop(0)
 A0.pop(0)
 B0.pop(0)
-kcc0.pop(0)
 label0.pop(0)
 # print(label0)
 # print(A0)
 # print(B0)
 
 # scatterplot
-plt.scatter(A1, B1, label="Label: 1", color='blue', s=15)
-plt.scatter(A0, B0, label="Label: 0", color='red', s=15)
-plt.xlabel('A')
-plt.ylabel('B')
-plt.title("chessboard data")
-plt.legend()
-plt.show()
+# plt.scatter(A1, B1, label="Label: 1", color='blue', s=15)
+# plt.scatter(A0, B0, label="Label: 0", color='red', s=15)
+# plt.xlabel('A')
+# plt.ylabel('B')
+# plt.title("chessboard data")
+# plt.legend()
+# plt.show()
 
 # K Neighbors Classifier
 from sklearn.neighbors import KNeighborsClassifier
-neigh = KNeighborsClassifier(n_neighbors=3)
-neigh.fit(kcc, label)
+neigh1 = KNeighborsClassifier(n_neighbors=1)
+neigh5 = KNeighborsClassifier(n_neighbors=5)
+neigh10 = KNeighborsClassifier(n_neighbors=10)
+neigh15 = KNeighborsClassifier(n_neighbors=15)
+
+# neigh.fit(all_samples, label)
 
 from sklearn import svm
+linear_svc = svm.SVC(kernel='linear')
+linear_svc.kernel
+linear_svc.fit(all_samples, all_labels)
+
+rbf_svc = svm.SVC(kernel='rbf')
+rbf_svc.kernel
+rbf_svc.fit(all_samples, all_labels)
+
+sigmoid_svc = svm.SVC(kernel='sigmoid')
+sigmoid_svc.kernel
+sigmoid_svc.fit(all_samples, all_labels)
+
+polynomial_svc = svm.SVC(kernel='poly')
+polynomial_svc.kernel
+polynomial_svc.fit(all_samples, all_labels)
+
+#
+# linear_svc = svm.SVC(kernel='linear')
+# linear_svc = svm.SVC(kernel='linear')
+#
+# clf = svm.SVC()
+# clf.fit(all_samples, all_labels)
+from sklearn.tree import DecisionTreeClassifier
+clf1 = tree.DecisionTreeClassifier(max_depth=1)
+clf1.fit(all_samples, all_labels)
+
+# print("now predictions" + clf.predict([[2,5]])[0])
 
 from sklearn.model_selection import train_test_split
+
 
 # print(neigh.predict([[1.1,3]]))
 # print(neigh.predict([[1.1,666663]]))
