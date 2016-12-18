25c88b1996abfb8e8aaeae861110b8aa26113b44
Rob <robertipk@gmail.com>
2016-12-17 20:37:29 -0500

    finished

--- classify.py
+++ classify.py
@@ -2,26 +2,21 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 from sklearn import svm, model_selection as ms, neighbors, tree
 import csv
 
-
 chessdata = pd.read_csv("chessboard.csv")
-# print(chessdata)
 a = chessdata[['A','B']]
 b = chessdata['label']
 
-# print(a)
-
 data = csv.reader(open('chessboard.csv','rb'), delimiter=",", quotechar='|')
-# for scatter plot
-A1, B1, A0, B0, all_samples, label1, label0 = [], [], [], [], [], [], []
-# for SVM
-all_labels = []
-
-# print(data)
+A1, B1, A0, B0, all_samples, label1, label0, all_labels = [], [], [], [], [], [], [], []
+# skip headers
+next(data, None)
 for row in data:
-
     all_samples.append([row[0],row[1]])
     all_labels.append(row[2])
     if row[2]=='0':
@@ -32,18 +27,6 @@ for row in data:
       A1.append(row[0])
       B1.append(row[1])
       label1.append(row[2])
-all_samples.pop(0)
-all_labels.pop(0)
-
-A1.pop(0)
-B1.pop(0)
-label1.pop(0)
-A0.pop(0)
-B0.pop(0)
-label0.pop(0)
-# print(label0)
-# print(A0)
-# print(B0)
 
 # scatterplot
 # plt.scatter(A1, B1, label="Label: 1", color='blue', s=15)
@@ -54,48 +37,50 @@ label0.pop(0)
 # plt.legend()
 # plt.show()
 
+# 10-fold cross-validation with mean and standard deviation
+def kCrossValidation(model, classifier, samples, labels):
+  print("Classifier: " + model)
+  scores = cross_val_score(classifier, all_samples, all_labels, cv=10)
+  print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
 # K Neighbors Classifier
-from sklearn.neighbors import KNeighborsClassifier
-neigh1 = KNeighborsClassifier(n_neighbors=1)
-neigh5 = KNeighborsClassifier(n_neighbors=5)
-neigh10 = KNeighborsClassifier(n_neighbors=10)
-neigh15 = KNeighborsClassifier(n_neighbors=15)
+knn1 = neighbors.KNeighborsClassifier(n_neighbors=1)
+kCrossValidation("K Nearest Neighbors: 1", knn1, all_samples, all_labels)
 
-# neigh.fit(all_samples, label)
+knn5 = KNeighborsClassifier(n_neighbors=5)
+kCrossValidation("K Nearest Neighbors: 5", knn5, all_samples, all_labels)
 
-from sklearn import svm
+knn10 = KNeighborsClassifier(n_neighbors=10)
+kCrossValidation("K Nearest Neighbors: 10", knn10, all_samples, all_labels)
+
+knn15 = KNeighborsClassifier(n_neighbors=15)
+kCrossValidation("K Nearest Neighbors: 15", knn15, all_samples, all_labels)
+
+# SVM
 linear_svc = svm.SVC(kernel='linear')
-linear_svc.kernel
-linear_svc.fit(all_samples, all_labels)
+kCrossValidation("Linear SVC", linear_svc, all_samples, all_labels)
 
 rbf_svc = svm.SVC(kernel='rbf')
-rbf_svc.kernel
-rbf_svc.fit(all_samples, all_labels)
+kCrossValidation("RBF SVC", rbf_svc, all_samples, all_labels)
 
 sigmoid_svc = svm.SVC(kernel='sigmoid')
-sigmoid_svc.kernel
-sigmoid_svc.fit(all_samples, all_labels)
+kCrossValidation("Sigmoid SVC", sigmoid_svc, all_samples, all_labels)
 
 polynomial_svc = svm.SVC(kernel='poly')
-polynomial_svc.kernel
-polynomial_svc.fit(all_samples, all_labels)
+kCrossValidation("Polynomial SVC", polynomial_svc, all_samples, all_labels)
 
-#
-# linear_svc = svm.SVC(kernel='linear')
-# linear_svc = svm.SVC(kernel='linear')
-#
-# clf = svm.SVC()
-# clf.fit(all_samples, all_labels)
-from sklearn.tree import DecisionTreeClassifier
-clf1 = tree.DecisionTreeClassifier(max_depth=1)
-clf1.fit(all_samples, all_labels)
+# Decision tree
+dt1 = tree.DecisionTreeClassifier(max_depth=1)
+kCrossValidation("Decision Tree - max depth 1", dt1, all_samples, all_labels)
 
-# print("now predictions" + clf.predict([[2,5]])[0])
+dt2 = tree.DecisionTreeClassifier(max_depth=2)
+kCrossValidation("Decision Tree - max depth 2", dt2, all_samples, all_labels)
 
-from sklearn.model_selection import train_test_split
+dt4 = tree.DecisionTreeClassifier(max_depth=4)
+kCrossValidation("Decision Tree - max depth 4", dt4, all_samples, all_labels)
 
+dt8 = tree.DecisionTreeClassifier(max_depth=8)
+kCrossValidation("Decision Tree - max depth 8", dt8, all_samples, all_labels)
 
-# print(neigh.predict([[1.1,3]]))
-# print(neigh.predict([[1.1,666663]]))
-
-# print(neigh.predict_proba([[0.9]]))
+dt16 = tree.DecisionTreeClassifier(max_depth=16)
+kCrossValidation("Decision Tree - max depth 16", dt16, all_samples, all_labels)
